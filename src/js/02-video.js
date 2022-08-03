b5ac894a3eb80f81dc4d53798ee47e46f7a142ae
Max Denishchuk <maks.denishchuk@gmail.com>
2022-08-03 11:45:30 +0300

    C:/Program Files/Git/

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+player.on('timeupdate', throttle(videoData, 1000));
+
+function videoData(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const { email = '', message = '' } = JSON.parse(
+  localStorage.getItem(STORAGE_KEY)
+);
+
+if (localStorage.getItem(STORAGE_KEY) !== null) {
+  formRef.email.value = email;
+  formRef.message.value = message;
+}
+
+const form = {
+  email,
+  message,
+};
+
+formRef.addEventListener(
+  'input',
+  throttle(e => {
+    switch (e.target.name) {
+      case 'email':
+        form.email = e.target.value;
+        break;
+      case 'message':
+        form.message = e.target.value;
+        break;
+      default:
+        break;
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(form));
+  }, 500)
+);
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  formRef.email.value = '';
+  formRef.message.value = '';
+  localStorage.setItem(STORAGE_KEY, '');
+});
